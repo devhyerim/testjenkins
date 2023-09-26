fa8096541c0c3dd62e901b5ea7c5206b7bfe0088
devhyerim <invisiblebink@gmail.com>
2023-09-26 12:04:42 +0900

    dfg

--- src/main/java/com/test/jenkins/HelloController.java
+++ src/main/java/com/test/jenkins/HelloController.java
@@ -3,13 +3,14 @@ package com.test.jenkins;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
 	@GetMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("message", "Hello jenkins");
+		//model.addAttribute("message", "Hello jenkins");
 		
 		return "hello";
 	}
